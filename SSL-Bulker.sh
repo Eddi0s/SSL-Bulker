79223dc2926dce8c184888d097e410b3b0558447
Eddi0s <root@DESKTOP-KBGT5K3.localdomain>
2022-12-19 22:40:46 +0100

    Fixed CertName formatting

--- SSL-Bulker.sh
+++ SSL-Bulker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-BREED=$(wc -L host.list | awk '{print $1}')
-DNSBREED=$(wc -L host.list | awk '{print $1 + 5}')
+BREED=$(wc -L host.list | awk '{print $1 + 5}')
+CertNameBreed=$(wc -L host.list | awk '{print $1 + 5}')
 
 echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-printf "%-${BREED}s %-20s %-40s -%-40s %-${DNSBREED}s %-20s %-20s\n" "Domain" "IP" "Server" "DNS" "Cert Name" "CA's" "Exp Date"
+printf "%-${BREED}s %-20s %-40s %-30s %-${CertNameBreed}s %-20s %-20s\n" "Domain" "IP" "Server" "DNS" "Cert Name" "CA's" "Exp Date"
 echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 while read Domain; do
@@ -16,7 +16,7 @@ CertName=$(echo "Q" | openssl s_client -connect $Domain:443 2>&1 | openssl x509 
 CA=$(echo "Q" | openssl s_client -servername $Domain -connect $Domain:443 2>/dev/null | grep -m 1 'O =' | cut -d, -f4 | cut -c 6- | awk '{print $1;}')
 DNS=$(dig ns $Domain | grep -A 3  ';; ANSWER SECTION:' | head -2 | tail -1 | awk '{ print $5}')
 
-printf "%-${BREED}s %-20s %-40s %-30s %-36s %-20s %-20s\n" "$Domain" "$IP" "$SERVER" "$DNS" "$CertName" "$CA" "$ExpDate"
+printf "%-${BREED}s %-20s %-40s %-30s %-${CertNameBreed}s %-20s %-20s\n" "$Domain" "$IP" "$SERVER" "$DNS" "$CertName" "$CA" "$ExpDate"
 
 done <host.list
 
