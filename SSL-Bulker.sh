82156d6c098e9322180d83cadaf9ab6f85572f75
Eddi0s <eddios@DESKTOP-KKNM3R2.localdomain>
2022-12-22 14:25:28 +0100

    Timeout fix

--- SSL-Bulker.sh
+++ SSL-Bulker.sh
@@ -11,9 +11,28 @@ while read Domain; do
 IP=$(host $Domain | awk 'NR==1{print $4}')
 SERVER=$(host $IP | awk 'NR==1{print $5}')
 SERVER=${SERVER%.*}
-ExpDate=$(echo "Q" | openssl s_client -servername $Domain -connect $Domain:443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -c 10-)
-CertName=$(echo "Q" | openssl s_client -connect $Domain:443 2>&1 | openssl x509 -noout -text |  awk '/Subject: C=/{printf $NF"\n"} /DNS:/{x=gsub(/ *DNS:/, ""); printf "SANS=" $0"\n"}' | cut -c 6- | cut -f1 -d"," )
-CA=$(echo "Q" | openssl s_client -servername $Domain -connect $Domain:443 2>/dev/null | grep -m 1 'O =' | cut -d, -f4 | cut -c 6- | awk '{print $1;}')
+
+
+
+ExpDate=$(timeout 1s bash -c "echo 'Q' | openssl s_client -servername $Domain -connect $Domain:443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -c 10-")
+
+if [ "$ExpDate" = "" ]; then
+  ExpDate="TimeOut"
+fi
+
+CertName=$(timeout 1s bash -c "echo 'Q' | openssl s_client -connect $Domain:443 2>&1 | openssl x509 -noout -text" | sed -n 11p | sed 's/^.*=//' | sed -r 's/^.{1}//' )
+
+if [ "$CertName" = "" ]; then
+  CertName="TimeOut"
+fi
+
+
+CA=$(timeout 1s bash -c "echo "Q" | openssl s_client -servername $Domain -connect $Domain:443 2>/dev/null" | grep -m 1 'O =' | cut -d, -f4 | cut -c 6- | awk '{print $1;}')
+
+if [ "$CA" = "" ]; then
+  CA="TimeOut"
+fi
+
 DNS=$(dig ns $Domain | grep -A 3  ';; ANSWER SECTION:' | head -2 | tail -1 | awk '{ print $5}')
 
 printf "%-${BREED}s %-20s %-40s %-30s %-${CertNameBreed}s %-20s %-20s\n" "$Domain" "$IP" "$SERVER" "$DNS" "$CertName" "$CA" "$ExpDate"
