8f4edfb48b9db510d5d18156244a538439744c56
Eddi0s <eddios@DESKTOP-KKNM3R2.localdomain>
2023-01-31 22:13:40 +0100

    Host Fix

--- SSL-Bulker.sh
+++ SSL-Bulker.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Defineer rood-kleur
+RED=$(tput setaf 1)
+
+# Defineer groen-kleur
+GREEN=$(tput setaf 2)
+# Defineer standaard kleur
+RESET=$(tput sgr0)
+
 BREED=$(wc -L host.list | awk '{print $1 + 5}')
 CertNameBreed=$(wc -L host.list | awk '{print $1 + 5}')
 
@@ -11,9 +19,14 @@ printf "%-${BREED}s %-20s %-40s %-30s %-${CertNameBreed}s %-20s %-20s\n" "Domain
 echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
 while read Domain; do
+
 IP=$(host $Domain | awk 'NR==1{print $4}')
+if [ -z "$IP" ]; then
+IP="${RED}Not Found"
+SERVER="${RED}Not Found${RESET}"
+else
 SERVER=$(host $IP | awk 'NR==1{print $5}')
-SERVER=${SERVER%.*}
+fi
 
 # Voer het dig commando uit en sla het resultaat op in een variabele
 DNS=$(dig ns $Domain)
@@ -26,14 +39,6 @@ if echo "$DNS" | grep -q ";; AUTHORITY SECTION:"; then
 elif echo "$DNS" | grep -q ";; ANSWER SECTION"; then
   DNS=$(echo "$DNS" | sed -n '/;; ANSWER SECTION/,+1p' | tail -n 1 | awk '{print $5;}')
 fi
-
-# Defineer rood-kleur
-RED=$(tput setaf 1)
-
-# Defineer groen-kleur
-GREEN=$(tput setaf 2)
-# Defineer standaard kleur
-RESET=$(tput sgr0)
 
 # Sla ExpDate output op in ExpDate variabele
 ExpDate=$(timeout 2s bash -c "echo 'Q' | openssl s_client -servername $Domain -connect $Domain:443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -c 10-")
@@ -62,13 +67,13 @@ fi
 CertName=$(timeout 2s bash -c "echo 'Q' | openssl s_client -connect $Domain:443 2>&1 | openssl x509 -noout -text" | sed -n 11p | sed 's/^.*=//' | sed -r 's/^.{1}//' )
 
 if [ "$CertName" = "" ]; then
-  CertName="${RED}TimeOut${RESET}"
+  CertName="${RED}TimeOut"
 fi
 
 CA=$(timeout 2s bash -c "echo "Q" | openssl s_client -servername $Domain -connect $Domain:443 2>/dev/null" | grep -m 1 'O =' | cut -d, -f4 | cut -c 6- | awk '{print $1;}')
 
 if [ "$CA" = "" ]; then
-  CA="${RED}CA not found${RESET}"
+  CA="${RED}CA not found"
 fi
 
 printf "%-${BREED}s %-20s %-40s %-30s %-${CertNameBreed}s %-20s %-20s\n" "$Domain" "$IP" "$SERVER" "$DNS" "$CertName" "$CA" "$ExpDate"
