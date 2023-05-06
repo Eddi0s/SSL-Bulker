5651bd131dc9a27e15f9ca515645fc419ae5174c
Eddi0s <edgarvdeng@gmail.com>
2023-05-06 18:11:46 +0200

    Update SSL-Bulker.sh

--- SSL-Bulker.sh
+++ SSL-Bulker.sh
@@ -1,90 +1,80 @@
 #!/bin/bash
 
-# Defineer rood-kleur
-RED=$(tput setaf 1)
+# This script reads a list of domain names from the file "host.list" and performs various checks and queries for each domain.
+# The output of each query is formatted into a table for easy readability.
 
-# Defineer groen-kleur
-GREEN=$(tput setaf 2)
-# Defineer standaard kleur
-RESET=$(tput sgr0)
+# Calculate the required padding for the "Width" and "CertNameWidth" columns in the output table.
+Width=$(wc -L host.list | awk '{print $1 + 5}')
+CertNameWidth=$(wc -L host.list | awk '{print $1 + 5}')
 
-BREED=$(wc -L host.list | awk '{print $1 + 5}')
-CertNameBreed=$(wc -L host.list | awk '{print $1 + 5}')
-
-# Noteer het starttijdstip
+# Get the current timestamp for calculating the script's runtime.
 start_time=$(date +%s)
 
-echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
-printf "%-${BREED}s %-20s %-40s %-30s %-${CertNameBreed}s %-20s %-20s\n" "Domain" "IP" "Server" "DNS" "Cert Name" "CA's" "Exp Date"
-echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+# Print the header row for the output table.
+echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"printf "%-${Width}s %-20s %-40s %-30s %-${CertNameWidth}s %-20s %-20s\n" "Domain" "IP" "Server" "DNS" "Cert Name" "CA's" "Exp Date"
+echo "----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
+# Loop through each line (domain) in the host.list file.
+while read -r Domain; do
 
-while read Domain; do
+ # Query the domain's IP address and server name.
+ IP=$(host $Domain | grep -m1 -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
+ if [ -z "$IP" ]; then
+  IP="NOT FOUND"
+  SERVER="NOT FOUND"
+ else
+ SERVER=$(host $IP | awk 'NR==1{print $5}' | cut -c1-35 | sed 's/\.$//')
+ fi
 
-IP=$(host $Domain | grep -m1 -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
-if [ -z "$IP" ]; then
-IP="${RED}Not Found"
-SERVER="${RED}Not Found${RESET}"
-else
-SERVER=$(host $IP | awk 'NR==1{print $5}')
-fi
+  # Query the domain's DNS name.
+  DNS=$(dig ns $Domain | grep -m1 -E "IN\s*(NS|SOA)\s" | awk '{ print $5 }' | cut -c1-30 | sed 's/\.$//' )
 
-# Voer het dig commando uit en sla het resultaat op in een variabele
-DNS=$(dig ns $Domain | grep -m1 -E "IN\s*(NS|SOA)\s" | awk '{ print $5 }')
+  # If the domain's DNS name is "ns1.dns.nl.", consider it as not found.
+  if [ "$DNS" = "ns1.dns.nl." ]; then
+    DNS="NOT FOUND"
+  fi
 
-# Sla ExpDate output op in ExpDate variabele
-ExpDate=$(timeout 2s bash -c "echo 'Q' | openssl s_client -servername $Domain -connect $Domain:443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -c 10-")
+  # Query the domain's SSL certificate name and extract it from the response.
+  CertName=$(echo | openssl s_client -servername $Domain -showcerts -connect $Domain:443 2>/dev/null)
+  if echo "$CertName" | grep -q "BEGIN CERTIFICATE"; then
+    CertName=$(echo "$CertName" | openssl x509 -noout -subject | awk '{print $NF}')
+  else
+    CertName="NOT FOUND"
+  fi
 
-# Parseer de output van de ExpDate variabele en sla de datum op in de EXP_DATE_PARSED variabele
-EXP_DATE_PARSED=$(date -d "$ExpDate" +%s)
+  # Query the domain's SSL certificate's CA name and extract it from the response.
+  CA=$(echo | openssl s_client -servername "$Domain" -showcerts -connect "$Domain":443 2>/dev/null)
+  if echo "$CA" | grep -q "BEGIN CERTIFICATE"; then
+    CA=$(echo "$CA" | openssl x509 -noout -issuer | awk -F= '/CN =/{print $NF}' | awk '{print $1}')
+  else
+    CA="NOT FOUND"
+  fi
 
-# Haal de huidige datum op en sla deze op in de CURRENT_DATE variabele
-CURRENT_DATE=$(date +%s)
+  # Query the domain's SSL expiration date and extract it from the response.
+  ExpDate=$(timeout 2s bash -c "echo 'Q' | openssl s_client -servername $Domain -connect $Domain:443 2>/dev/null | openssl x509 -noout -dates 2>/dev/null")
 
-# Bereken het aantal seconden tot twee maanden verval
-TWO_MONTHS_IN_SECONDS=$((60*60*24*60))
+  # If the output of openssl s_client contains the string "notAfter", extract the expiration date and assign it to the ExpDate variable
+  if echo "$ExpDate" | grep -q "notAfter"; then
+   ExpDate=$(echo "$ExpDate" | grep notAfter | cut -c 10-)
+  else
+  # If the output does not contain "notAfter", set the expiration date to "NOT FOUND"
+   ExpDate="NOT FOUND"
+  fi
 
-if [ "$ExpDate" = "" ]; then
- ExpDate="${RED}TimeOut${RESET}"
-fi
+# Print the domain, IP address, server name, DNS name, certificate name, CA, and expiration date
+printf "%-${Width}s %-20s %-40s %-30s %-${CertNameWidth}s %-20s %-20s\n" "$Domain" "$IP" "$SERVER" "$DNS" "$CertName" "$CA" "$ExpDate"
 
-# Als de vervaldatum binnen twee maanden valt, kleur de output rood
-if [ $((EXP_DATE_PARSED - CURRENT_DATE)) -lt $TWO_MONTHS_IN_SECONDS ]; then
-  ExpDate="${RED}$ExpDate${RESET}"
-else
-  # Als de vervaldatum langer dan twee maanden in de toekomst ligt, kleur de output groen
-  ExpDate="${GREEN}$ExpDate${RESET}"
-fi
-
-CertName=$(timeout 2s bash -c "echo 'Q' | openssl s_client -connect $Domain:443 2>&1 | openssl x509 -noout -text" | sed -n 11p | sed 's/^.*=//' | sed -r 's/^.{1}//' )
-
-if [ "$CertName" = "" ]; then
-  CertName="${RED}TimeOut"
-fi
-
-CA=$(timeout 2s bash -c "echo "Q" | openssl s_client -servername $Domain -connect $Domain:443 2>/dev/null" | grep -m 1 'O =' | grep -o 'O = [^,]*' | sed 's/O = //')
-
-if [ "$CA" = "" ]; then
-  CA="CA not found"
-fi
-
-if [ "$CA" = "Let's Encrypt" ]; then
-  CA="Lets Encrypt"
-fi
-
-printf "%-${BREED}s %-20s %-40s %-30s %-${CertNameBreed}s %-20s %-20s\n" "$Domain" "$IP" "$SERVER" "$DNS" "$CertName" "$CA" "$ExpDate"
-
+# Loop through each domain in the "host.list" file
 done <host.list
 
+# Print a separator line
 echo "-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------"
 
+# Print the current date and time
 echo "Scanned on $(date)"
 
-# Noteer het eindtijdstip
+# Calculate the total time the script took to run and print it
 end_time=$(date +%s)
-
-# Bereken het verschil tussen het eind- en starttijdstip
 elapsed_time=$((end_time - start_time))
 
-# Weergeef het verschil in seconden
+# Print elapsed time for runtime script
 echo "Script took $elapsed_time seconds to finish"
-
